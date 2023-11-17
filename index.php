af3751a0190b7220f746d61b306f818bab567a55
MaulanaBintang <117064989+MaulanaBintang@users.noreply.github.com>
2023-11-17 20:27:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,104 +7,215 @@
 <!-- HTML -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title>The Elite</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css">
+    <link rel="stylesheet" href="index.css">
     <link rel="icon" href="img/general/theelitelogo.png" type="image/x-icon" />
-    <link rel="stylesheet" href="style.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>The Elite</title>
 </head>
 <body>
-    <!-- NAVBAR -->
-   <header class="header">
-    <nav class="navbar">
-        <a href="index.php">Home</a>
-        <a href="#">Market</a>
-          <a href="About.html">About</a>
-          <a href="help.html">Help</a>
+    <nav>
+        <div class="nav-logo">
+            <a href="#">
+                <img src="img/general/theelitelogo.png">
+            </a>
+        </div>
+
+        <ul class="nav-links">
+            <li class="link"><a href="#">Home</a></li>
+            <li id="link1" class="link"><a href="market.php">Market</a></li>
+            <li id="link2" class="link"><a href="contact.php">Contact</a></li>
+            <li id="link3" class="link"><a href="about.php">About</a></li>
+            
+        </ul>
     </nav>
-    <form action="" class="search-bar">
-       <input type="text" placeholder="Search...">
-       <button><i class='bx bx-search'></i></button>
-    </form>
-   </header>
-    <!-- LOGIN FORM -->
-    <div class="background"></div>
-    <div class="container">
-        <div class="item">
-            <h2 class="logo"><i class='bx bxl-xing'></i>The Elite</h2>
-            <div class="text-item">
-                <h2>Welcome! <br><span>
-                    To Our Organisation!
-                </span></h2>
-                <p>Grasp Luxury, Touch Quality</p>
-                <div class="social-icon">
-                    <a href="#"><i class='bx bxl-facebook'></i></a>
-                    <a href="#"><i class='bx bxl-twitter'></i></a>
-                    <a href="#"><i class='bx bxl-youtube'></i></a>
-                    <a href="#"><i class='bx bxl-instagram'></i></a>
-                    <a href="#"><i class='bx bxl-linkedin'></i></a>
+
+    <header class="container">
+        <div class="content">
+            <span class="blur"></span>
+            <span class="blur"></span>
+            <h4>This Is Your Chance</h4>
+            <H1>Welcome To Our Organization <span>The Elite</span></H1>
+            <p>
+            Bergabunglah dengan komunitas yang menghargai keindahan dan keunggulan. 
+            The Elite bukan hanya sekadar e-commerce, tetapi panggung bagi gaya hidup Anda. 
+            Selamat berbelanja di dunia yang dirancang khusus untuk Anda, di The Elite
+            </p>
+            <button class="btn"><a href="login.php">Login</a></button>
+            
+        </div>
+        <div class="image">
+            <img src="img/general/index.png">
+        </div>
+    </header>
+
+    <section class="container">
+        <h2 class="header">OUR ADVANTAGES</h2>
+        <div class="features">
+            <div class="card">
+                <span><i class="ri-money-dollar-box-line"></i></span>
+                <h4>Exclusive Collection</h4>
+                <p>
+                The Elite menyajikan koleksi produk eksklusif dari merek-merek paling bergengsi di seluruh dunia. 
+                Kami memilih dengan cermat setiap item untuk memastikan bahwa hanya yang terbaik yang dapat
+                </p>
+                <a href="#">Join Now <i class="ri-arrow-right-line"></i></a>
+            </div>
+            <div class="card">
+                <span><i class="ri-bug-line"></i></span>
+                <h4>Technological Innovation</h4>
+                <p>
+                The Elite selalu mengikuti perkembangan teknologi terbaru. Dengan antarmuka yang responsif dan fitur-fitur inovatif
+                </p>
+                <a href="#">Join Now <i class="ri-arrow-right-line"></i></a>
+            </div>
+            <div class="card">
+                <span><i class="ri-time-line"></i></span>
+                <h4>24/7 Customer Service</h4>
+                <p>
+                Apakah Anda memiliki pertanyaan tentang produk, pengiriman, atau masalah lainnya, 
+                kami berkomitmen untuk memberikan pelayanan pelanggan terbaik 24 jam sehari, 7 hari seminggu.
+                </p>
+                <a href="#">Join Now <i class="ri-arrow-right-line"></i></a>
+            </div>
+            <div class="card">
+                <span><i class="ri-shake-hands-line"></i></span>
+                <h4>Exclusive Membership Program</h4>
+                <p>
+                Bergabunglah dengan program keanggotaan eksklusif kami untuk menikmati berbagai keuntungan, 
+                termasuk diskon khusus, akses awal ke koleksi terbaru, dan pengalaman berbelanja yang lebih personal.
+                </p>
+                <a href="#">Join Now <i class="ri-arrow-right-line"></i></a>
+            </div>
+        </div>
+    </section>
+
+    <section class="container">
+        <h2 class="header">PRICING PLANS</h2>
+        <p class="sub-header">
+            Our pricing plans come with various membership tiers, each tailored to cater to different type of investment and
+            type of member.
+        </p>
+        <div class="pricing">
+            <div class="card">
+                <div class="content">
+                    <h4>Basic Member</h4>
+                    <h3>5$</h3>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Diskon khusus 5% untuk semua produk.
+                    </p>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Layanan pelanggan prioritas.
+                    </p>
                 </div>
+                <button class="btn">Join Now</button>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h4>Gold Member</h4>
+                    <h3>50$</h3>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Diskon khusus 10% untuk semua produk.
+                    </p>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Akses awal ke penjualan khusus dan koleksi terbaru.
+                    </p>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Layanan pelanggan prioritas.
+                    </p>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Akses eksklusif ke penawaran produk spesial
+                    </p>
+                </div>
+                <button class="btn">Join Now</button>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <h4>Platinum Member</h4>
+                    <h3>100$</h3>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Diskon khusus 25% untuk semua produk.
+                    </p>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Akses awal ke penjualan khusus dan koleksi terbaru.
+                    </p>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Layanan pelanggan prioritas.
+                    </p>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Akses eksklusif ke penawaran produk spesial
+                    </p>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Konsultan gaya pribadi untuk membantu Anda memilih produk
+                    </p>
+                    <p>
+                        <i class="ri-checkbox-circle-line"></i>
+                        Undangan eksklusif ke acara dan pengalaman VIP.
+                    </p>
+                </div>
+                <button class="btn">Join Now</button>
             </div>
         </div>
-        <!-- Login Box -->
-        <div class="login-section">
-            <div class="form-box login">
-                <form action="">
-                    <h2>Sign In</h2>
-                    <div class="input-box">
-                        <span class="icon"><i class='bx bxs-envelope'></i></span>
-                        <input type="email" name="email" id="email" placeholder="email" required>
-                        <label >Email</label>
-                    </div>
-                    <div class="input-box">
-                        <span class="icon"><i class='bx bxs-lock-alt' ></i></span>
-                        <input type="password" name="password" id="password" required>
-                        <label>Password</label>
-                    </div>
-                    <div class="remember-password">
-                        <label for=""><input type="checkbox">Remember Me</label>
-                        <a href="#">Forget Password</a>
-                    </div>
-                    <button class="btn">Login In</button>
-                    <div class="create-account">
-                        <p>Create A New Account? <a href="#" class="register-link">Sign Up</a></p>
-                    </div>
-                </form>
+    </section>
+
+    <footer class="container">
+        <span class="blur"></span>
+        <span class="blur"></span>
+        <div class="column">
+            <div class="logo">
+                <img src="img/general/theelitelogo.png">
             </div>
-            <div class="form-box register">
-                <form action="">
-
-                    <h2>Sign Up</h2>
-
-                    <div class="input-box">
-                        <span class="icon"><i class='bx bxs-user'></i></span>
-                        <input type="text" required>
-                        <label >Username</label>
-                    </div>
-                    <div class="input-box">
-                        <span class="icon"><i class='bx bxs-envelope'></i></span>
-                        <input type="email" required>
-                        <label >Email</label>
-                    </div>
-                    <div class="input-box">
-                        <span class="icon"><i class='bx bxs-lock-alt' ></i></span>
-                        <input type="password" required>
-                        <label>Password</label>
-                    </div>
-                    <div class="remember-password">
-                        <label for=""><input type="checkbox">I agree with this statment</label>
-                    </div>
-                    <button class="btn">Register</button>
-                    <div class="create-account">
-                        <p>Already Have An Account? <a href="#" class="login-link">Sign In</a></p>
-                    </div>
-                </form>
+            <p>
+            Grasp Luxury, Touch Quality
+            </p>
+            <div class="socials">
+                <a href="#"><i class="ri-facebook-line"></i></a>
+                <a href="#"><i class="ri-instagram-line"></i></a>
+                <a href="#"><i class="ri-twitter-line"></i></a>
+                <a href="#"><i class="ri-linkedin-line"></i></a>
+                
             </div>
         </div>
+        <div class="column">
+            <h4>Company</h4>
+            <a href="#">Business</a>
+            <a href="#">Partnership</a>
+            <a href="#">Network</a>
+        </div>
+        <div class="column">
+            <h4>About Us</h4>
+            <a href="#">Blogs</a>
+            <a href="#">Channels</a>
+            <a href="#">Sponsors</a>
+        </div>
+        <div class="column">
+            <h4>Contact</h4>
+            <a href="#">Contact Us</a>
+            <a href="#">Privicy Policy</a>
+            <a href="#">Terms & Conditions</a>
+        </div>
+    </footer>
+
+    <div class="copyright">
+        Copyright © 2023 Maulana Bintang. All Rights Reserved.
     </div>
-     <!-- SIGN UP FORM CREATION -->
 
-    <script src="index.js"></script>
+
+    <script src="script.js"></script>
 </body>
 
 </html>
